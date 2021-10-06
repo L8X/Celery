694ecddd7e20eef6f090ee060f4ea1e7cd504070
thedoomed <46077863+thedoomed@users.noreply.github.com>
2021-10-05 20:57:40 -0400

    Update CeleryInit.lua

--- CeleryInit.lua
+++ CeleryInit.lua
@@ -4,15 +4,20 @@ celery.settings = {
   ["update"] = "d0e0f9747ed3455c"
 };
 
-version = get_app_path();
-version = version:sub(version:find("version-") + 8, string.len(version));
+celery.check_version = function()
+  version = get_app_path();
+  version = version:sub(version:find("version-") + 8, string.len(version));
 
-if (version ~= celery.settings.update) then
-  alert("Wrong ROBLOX version detected. Please wait for Celery to update...");
-  system("exit");
+  if (version ~= celery.settings.update) then
+    alert("Wrong ROBLOX version detected. Please wait for Celery to update...");
+    system("exit");
+    return false;
+  end
+  
+  return true;
 end
 
-if not whitelisted() then
+if celery.check_version() and not whitelisted() then
   alert("Unauthorized. Please send a new key to jayyy#8941 to start using Celery");
 end
 
