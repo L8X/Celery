837a163486a3c058c4fd859e85cee9a160dcabf0
thedoomed <46077863+thedoomed@users.noreply.github.com>
2021-10-05 20:27:25 -0400

    Update CeleryInit.lua

--- CeleryInit.lua
+++ CeleryInit.lua
@@ -1,9 +1,16 @@
-local celery = {};
+celery = {};
 
 celery.settings = {
-  ["version"] = "version-d0e0f9747ed3455c"
-  
+  ["update"] = "d0e0f9747ed3455c"
 };
+
+version = get_app_path();
+version = version:sub(version:find("version-") + 8, string.len(version));
+
+if (version ~= celery.settings.update) then
+  alert("Wrong ROBLOX version detected. Please wait for Celery to update...");
+  system("exit");
+end
 
 
 
